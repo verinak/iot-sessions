ed91b49e039184caf6136800aa20b14e40cbb227
marlymagdy <119417723+marlymagdy@users.noreply.github.com>
2023-07-18 20:51:07 +0300

    force.cpp

--- assignment 1/force.cpp
+++ assignment 1/force.cpp
@@ -3,8 +3,8 @@
 #include <cmath>
 using namespace std;
 
-const double UNIVERSAL_GRAVITATIONAL = 6.6743*pow(10,-8);
-double calc_force(double m1,double m2,double distance);
+const double UNIVERSAL_GRAVITATIONAL = 6.6743*pow(10,-8);    //define a constant constant global variable
+double calc_force(double m1,double m2,double distance);      //declare the calc_force function 
 
 int main()
 {   
@@ -24,7 +24,7 @@ int main()
         cout << "Enter d: " << endl;
         cin>>distance;
 
-        // calculate force using defined function and print it
+        // invoke the function and store the return value in variable of same type
         double force = calc_force(m1, m2, distance);
         cout<<"\nGravitational Force = "<<force << endl;
 
@@ -33,12 +33,13 @@ int main()
         cout << "\nWould you like to calculate again? (y/n) ";
         cin >> calcAgain;
     }
-    while(calcAgain == 'y');
+    while(calcAgain == 'y'); // if the user enters "n" the program will stop
     
     return 0;
 }
 
-double calc_force(double m1,double m2,double distance){
+double calc_force(double m1,double m2,double distance){       
+    //function definition outside the main
     double force = UNIVERSAL_GRAVITATIONAL*m1*m2/pow(distance,2);
     return force;
 }
