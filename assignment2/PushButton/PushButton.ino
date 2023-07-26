3a037e3300ee731b3a52d47be15e591dec32d483
Verina Michel <verinamichelk@gmail.com>
2023-07-26 03:01:49 +0300

    Update PushButton.ino

--- assignment2/PushButton/PushButton.ino
+++ assignment2/PushButton/PushButton.ino
@@ -1,6 +1,6 @@
-// define led and push button pins
 #define LED_PIN 2
 #define BUTTON_PIN 13
+// Define the pins used for the LED and button
 
 // setup code, to run once:
 void setup() {
@@ -16,19 +16,21 @@ void setup() {
 // main code, to run repeatedly:
 void loop() {
 
-  // read button input
-  // high == button is not peressed, low == button is pressed
   int buttonvalue = digitalRead(BUTTON_PIN);
+  // Read the value of the button
+  // high == button is not peressed, low == button is pressed
 
   if(buttonvalue == HIGH) {
+    // If it is not pressed, turn off the LED and print a message to the serial console
     Serial.println("not pressed");
     Serial.println(buttonvalue);
-    digitalWrite(LED_PIN, LOW); // turn off led if button is not pressed
+    digitalWrite(LED_PIN, LOW);
   }
   else {
+    // If it is pressed, turn on the LED and print a message to the serial console
     Serial.println("pressed");
     Serial.println(buttonvalue);
-    digitalWrite(LED_PIN, HIGH);  // turn on led if button is pressed
+    digitalWrite(LED_PIN, HIGH);
   }
   
 
