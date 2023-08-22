c9ac7fca1fb8ca36569ee1d912778beb3398324e
MariemNasr <119452857+MariemNasr@users.noreply.github.com>
2023-08-22 15:46:08 +0300

    Update servo_screen.dart

--- Flutter session 1/lib/part 1/ServoScreen.dart
+++ Flutter session 1/lib/part 1/ServoScreen.dart
@@ -1,32 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/login_screen.dart';
 
 class ServoScreen extends StatefulWidget {
-  const ServoScreen({super.key});
+  const ServoScreen({Key? key}) : super(key: key);
 
   @override
-  State<ServoScreen> createState() => _SliderExampleState();
+  State<ServoScreen> createState() => _ServoScreenState();
 }
 
-class _SliderExampleState extends State<ServoScreen> {
+class _ServoScreenState extends State<ServoScreen> {
   double _currentSliderValue = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Servo Angle')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text("Enter the Servo angle!", style: TextStyle(fontSize: 40)),
-          Text("The Angle: "+_currentSliderValue.toStringAsFixed(1), style: TextStyle(fontSize: 20)),
-          Slider(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/assets/images/img_3.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              "Enter the Servo angle!",
+              style: TextStyle(fontSize: 35),
+            ),
+            Text(
+              "Servo Angle: " + _currentSliderValue.toStringAsFixed(1),
+              style: TextStyle(fontSize: 20),
+            ),
+            Slider(
               value: _currentSliderValue,
               min: 0,
               max: 180,
               label: _currentSliderValue.toStringAsFixed(1),
-              //activeColor: Colors.lightBlue,
-              //inactiveColor: Colors.lightBlueAccent,
-              onChanged: (value){
+              onChanged: (value) {
+                setState(() {
+                  _currentSliderValue = value;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => LoginScreen()),
+          );
+        },
+        child: const Icon(Icons.account_circle_rounded),
+      ),
+    );
+  }
+}
                 setState(() {
                   _currentSliderValue = value;
                 });
